b56b17db4ba754f7b38a1e66b9ac1143687a5113
jdestin <jeremy.destin@inra.fr>
2018-11-21 15:55:37 +0100

    chore: Configure gradle for frontend build and test. Fix GitLab CI. GNP-5430.

---
+++ frontend/build.gradle.kts
@@ -0,0 +1,44 @@
+import com.moowork.gradle.node.npm.NpmTask
+
+plugins {
+    base
+    id("com.moowork.node") version "1.2.0"
+}
+
+val isCi = System.getenv("CI") != null
+
+node {
+    version = "10.13.0"
+    npmVersion = "6.4.1"
+    if (isCi) {
+        // we specify a custom installation directory because of permission issues on Docker
+        workDir = file("/tmp/node")
+    }
+    download = true
+}
+
+
+tasks {
+
+    val test by creating {
+        dependsOn("npm_run_lint")
+        dependsOn("npm_run_test")
+    }
+
+    val check by getting {
+        dependsOn(test)
+    }
+
+    val assemble by getting {
+        dependsOn("npm_run_lint")
+        dependsOn("npm_run_build")
+    }
+
+    val clean by getting {
+    }
+
+    val clientIntegrationTest by creating(NpmTask::class) {
+        dependsOn("npm_run_e2e")
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "gpds"
+include("frontend")
