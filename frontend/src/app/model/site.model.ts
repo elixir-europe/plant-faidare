48bed0ad30c2e6a1ecfe05139c57fce59a5695b4
Erik Kimmel <erik.kimmel@inra.fr>
2018-12-28 10:58:33 +0100

    feat: add fields to SiteModel. GNP-5433.

--- frontend/src/app/model/site.model.ts
+++ frontend/src/app/model/site.model.ts
@@ -1,15 +1,31 @@
-export class SiteModel {
+export interface SiteModel {
 
     result: SiteResultModel;
 
 }
 
-export class SiteResultModel {
+export interface SiteResultModel {
+
+    locationDbId: number;
+
+    name: string;
+
+    locationType: string;
+
+    abbreviation: string;
+
+    countryCode: string;
+
+    countryName: string;
+
+    institutionAdress: string;
+
+    institutionName: string;
+
+    altitude: number;
 
     latitude: number;
 
     longitude: number;
-
-    locationType: string;
 
 }
