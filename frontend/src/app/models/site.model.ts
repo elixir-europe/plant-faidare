9b02577e12f00c80b89b6234689696eeb6962353
Erik Kimmel <erik.kimmel@inra.fr>
2019-01-03 13:58:02 +0100

    feat: test if there are values before displaying the table line. GNP-5433.

--- frontend/src/app/models/site.model.ts
+++ frontend/src/app/models/site.model.ts
@@ -1,31 +1,27 @@
 export interface SiteModel {
-
     result: SiteResultModel;
-
 }
 
 export interface SiteResultModel {
-
     locationDbId: number;
-
     name: string;
-
     locationType: string;
-
     abbreviation: string;
-
     countryCode: string;
-
     countryName: string;
-
     institutionAdress: string;
-
     institutionName: string;
-
     altitude: number;
-
     latitude: number;
-
     longitude: number;
+    additionalInfo?: AdditionalInfo;
+}
 
+export interface AdditionalInfo {
+    Exposure: string;
+    'Distance to city': string;
+    Comment: string;
+    'Geographical location': string;
+    Slope: string;
+    'Coordinates precision': string;
 }
