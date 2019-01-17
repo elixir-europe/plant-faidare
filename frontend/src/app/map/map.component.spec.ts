1ee3c67ec1a741ac4f0ef6ee7ef5bd279b0295e6
Erik Kimmel <erik.kimmel@inra.fr>
2019-01-17 10:58:16 +0100

    test: properly initialize unit tests for the map component. Fix GNP-5433.

--- frontend/src/app/map/map.component.spec.ts
+++ frontend/src/app/map/map.component.spec.ts
@@ -1,25 +1,56 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapComponent } from './map.component';
+import { SiteModel } from '../models/site.model';
 
 describe('MapComponent', () => {
-  let component: MapComponent;
-  let fixture: ComponentFixture<MapComponent>;
+    let component: MapComponent;
+    let fixture: ComponentFixture<MapComponent>;
+    const site: SiteModel = {
+        result: {
+            locationDbId: 1,
+            latitude: 1,
+            longitude: 1,
+            altitude: 1,
+            institutionName: '',
+            institutionAdress: '',
+            countryName: '',
+            countryCode: '',
+            locationType: '',
+            abbreviation: '',
+            name: 'site1',
+            additionalInfo: {
+                Topography: '',
+                Slope: '',
+                Comment: '',
+                Exposure: '',
+                'Coordinates precision': '',
+                'Direction from city': '',
+                'Distance to city': '',
+                'Environment type': '',
+                'Geographical location': '',
+                'Site status': ''
+            }
+        }
+    };
+    const sites: Array<SiteModel> = new Array<SiteModel>();
+    sites.push(site);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MapComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MapComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MapComponent);
+        component = fixture.componentInstance;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        component.sites = sites;
+        fixture.detectChanges();
+        expect(component).toBeTruthy();
+    });
 });
