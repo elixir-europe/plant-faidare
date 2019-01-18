871c2d4afdf2cf4822dbe072ceb6c4cdf7569165
jdestin <jeremy.destin@inra.fr>
2019-01-18 17:49:29 +0100

    feat: Add files to display map. Install ngx-speculoos Minor fixes. GNP-5424.

--- frontend/src/app/study-card/study-card.component.spec.ts
+++ frontend/src/app/study-card/study-card.component.spec.ts
@@ -1,17 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
 import { StudyCardComponent } from './study-card.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentTester } from 'ngx-speculoos';
 
 describe('StudyCardComponent', () => {
-    let component: StudyCardComponent;
-    let fixture: ComponentFixture<StudyCardComponent>;
-    // let httpMock;
+    /*let component: StudyCardComponent;
+    let fixture: ComponentFixture<StudyCardComponent>;*/
+
+    class StudyCardComponentTester extends ComponentTester<StudyCardComponent> {
+        constructor() {
+            super(StudyCardComponent);
+        }
+    }
+
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [StudyCardComponent],
+
             schemas: [NO_ERRORS_SCHEMA]
         })
             .compileComponents();
@@ -23,8 +31,15 @@ describe('StudyCardComponent', () => {
             providers: [HttpClientTestingModule]
         });
 
-        fixture = TestBed.createComponent(StudyCardComponent);
+        /*fixture = TestBed.createComponent(StudyCardComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
+        fixture.detectChanges();*/
+    });
+
+    it('should create', () => {
+        const tester = new StudyCardComponentTester();
+        const component = tester.componentInstance;
+
+        expect(component).toBeTruthy();
     });
 });
