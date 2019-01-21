d0e8c52e32ffad52b08e6ac8f0c97de2b1ab4481
Erik Kimmel <erik.kimmel@inra.fr>
2019-01-21 13:31:11 +0100

    feat: use key/value pairs for additional informations. Remove commented/test code. GNP-5433.

--- frontend/src/app/models/site.model.ts
+++ frontend/src/app/models/site.model.ts
@@ -5,27 +5,18 @@ export interface SiteModel {
 export interface SiteResultModel {
     locationDbId: number;
     name: string;
-    locationType: string;
-    abbreviation: string;
-    countryCode: string;
-    countryName: string;
-    institutionAdress: string;
-    institutionName: string;
-    altitude: number;
-    latitude: number;
-    longitude: number;
+    locationType?: string;
+    abbreviation?: string;
+    countryCode?: string;
+    countryName?: string;
+    institutionAdress?: string;
+    institutionName?: string;
+    altitude?: number;
+    latitude?: number;
+    longitude?: number;
     additionalInfo?: AdditionalInfo;
 }
 
 export interface AdditionalInfo {
-    'Site status': string;
-    Exposure: string;
-    'Distance to city': string;
-    'Direction from city': string;
-    'Environment type': string;
-    Topography: string;
-    'Geographical location': string;
-    Slope: string;
-    'Coordinates precision': string;
-    Comment: string;
+    [key: string]: string;
 }
--- frontend/src/app/site-card/site-card.component.ts
+++ frontend/src/app/site-card/site-card.component.ts
@@ -14,6 +14,8 @@ export class SiteCardComponent implements OnInit {
 
     sites: Array<SiteModel> = new Array<SiteModel>();
 
+    additionalInfoKeys: string[] = [];
+
     loadingError = false;
 
     constructor(private brapiService: BrapiService, private route: ActivatedRoute) {
@@ -23,16 +25,13 @@ export class SiteCardComponent implements OnInit {
         // initialize site from location index
         const locationId = +this.route.snapshot.paramMap.get('id');
         this.brapiService.location(locationId).subscribe(
-          site => { this.site = site; this.sites.push(site); /* this.sites.push(this.getRandomSite(site)); */ },
+          site => {
+              this.site = site; this.sites.push(site);
+              if (site.result.additionalInfo) {
+                  this.additionalInfoKeys = Object.keys(site.result.additionalInfo);
+              }
+          },
           () => { console.log('Unable to load site...'); this.loadingError = true; }
         );
     }
-
-    getRandomSite(site: SiteModel): SiteModel {
-        const newSite = site;
-        newSite.result.latitude = site.result.latitude - 1;
-        newSite.result.longitude = site.result.longitude - 1;
-        return newSite;
-    }
-
 }
