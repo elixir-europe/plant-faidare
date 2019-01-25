a6d5ed287ddb309870aadcf3f965c0f5708192e2
Erik Kimmel <erik.kimmel@inra.fr>
2019-01-25 10:42:17 +0100

    feat: use a fully english map. GNP-5433.

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -21,8 +21,9 @@ export class MapComponent implements OnInit {
         const container = L.DomUtil.get('map');
         if (container) {
             const map = L.map('map').setView([firstSite.result.latitude, firstSite.result.longitude], 5);
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: 'INRA - URGI'
+            L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+                attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, ' +
+                    'Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
             }).addTo(map);
             // add markers for all sites using markercluster plugin
             const markers = new MarkerClusterGroup();
