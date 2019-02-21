1031a3ae4bd1f0bd260a63d464135b1125c44f80
gcornut <guillaume.cornut@inra.fr>
2019-02-21 16:56:25 +0100

    fix: Use font awesome spinner everywhere. #5

--- frontend/src/app/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/loading-spinner/loading-spinner.component.ts
@@ -1,18 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'gpds-loading-spinner',
-    templateUrl: './loading-spinner.component.html',
-    styleUrls: ['./loading-spinner.component.scss']
+    template: `
+        <i *ngIf="loading" class="fa fa-spin fa-spinner" title="Loading..."></i>
+    `,
+    styles: [`
+        i {
+            font-size: 2.2em;
+        }
+    `]
 })
-export class LoadingSpinnerComponent implements OnInit {
+export class LoadingSpinnerComponent {
 
     @Input() loading: boolean;
-
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
 
 }
