68df2e52ac9292185ba8eeb5e55c3b8b6324ce46
gcornut <guillaume.cornut@inra.fr>
2019-02-21 15:36:00 +0100

    chore: Update to Angular 7.2.6. Resolves npm audit warnings.
    Angular update done using `ng update @angular/core` and applying the
    relevant modifications form:
    https://github.com/cexbrayat/angular-cli-diff/compare/7.0.4...7.2.4

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
         },
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, '../coverage'),
-            reports: ['html', 'lcovonly'],
+            reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },
         reporters: ['progress', 'kjhtml', 'junit'],
