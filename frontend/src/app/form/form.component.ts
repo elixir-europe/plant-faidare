80b38d78e70bd9fa0fa28740de666fdaf705347f
gcornut <guillaume.cornut@inra.fr>
2019-03-28 16:11:36 +0100

    test: Add test for form tab switching and simplify form component code.

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -2,6 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DataDiscoveryCriteria } from '../models/data-discovery.model';
 import { BehaviorSubject } from 'rxjs';
 
+enum Tabs {
+    GERMPLASM = 'Germplasm',
+    TRAIT = 'Trait'
+}
+
 @Component({
     selector: 'gpds-form',
     templateUrl: './form.component.html',
@@ -10,5 +15,18 @@ import { BehaviorSubject } from 'rxjs';
 export class FormComponent {
     @Input() criteria$: BehaviorSubject<DataDiscoveryCriteria>;
     @Output() traitWidgetInitialized = new EventEmitter();
-    activeTab = 'Germplasm';
+
+    // Default active tab
+    activeTab: Tabs = Tabs.GERMPLASM;
+
+    // to give access in HTML template
+    tabs = Tabs;
+
+    getNavClass(tab: Tabs) {
+        return this.activeTab === tab ? 'active' : '';
+    }
+
+    getTabClass(tab: Tabs) {
+        return this.activeTab === tab ? 'visible' : 'd-none';
+    }
 }
