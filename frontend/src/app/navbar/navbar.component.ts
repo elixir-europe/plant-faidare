4765537eeaae905410afd7af7f87a89c5c7c7328
jdestin <jeremy.destin@inra.fr>
2019-06-11 19:49:23 +0200

    feat: Add back to form button. Issue #15

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { routes as appUrlRoutes } from '../app-routing.module';
+import { ActivatedRoute, Params, Router, RoutesRecognized } from '@angular/router';
 
 @Component({
     selector: 'faidare-navbar',
@@ -8,10 +10,40 @@ import { environment } from '../../environments/environment';
 })
 export class NavbarComponent {
 
+    displayBackButton = false;
+    previousQueryParam: Params;
+
+    constructor(private router: Router, private route: ActivatedRoute
+    ) {
+
+        this.router.events.subscribe(event => {
+            if (event instanceof RoutesRecognized) {
+                if (this.checkIsFormPage(event.url)) {
+                    this.displayBackButton = false;
+                    this.route.queryParams.subscribe(queryParam => {
+                        this.previousQueryParam = queryParam;
+                    });
+                } else {
+                    this.displayBackButton = true;
+                }
+            }
+        });
+    }
+
     navbar = environment.navbar;
     navbarCollapsed = true;
 
     toggleNavbar() {
         this.navbarCollapsed = !this.navbarCollapsed;
+    }
+
+    checkIsFormPage(url: String) {
+        for (const route of appUrlRoutes) {
+            const routeType: string = route.path.split(':')[0];
+            if (routeType !== '' && url.startsWith('/' + routeType)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
