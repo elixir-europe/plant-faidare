Ninja-Squad/faidare

feat: implement MIAPPE export of germplasms

backend/src/main/java/fr/inrae/urgi/faidare/web/germplasm/GermplasmController.java
@@ -1,5 +1,11 @@
 package fr.inrae.urgi.faidare.web.germplasm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import fr.inrae.urgi.faidare.api.NotFoundException;
 import fr.inrae.urgi.faidare.config.FaidareProperties;
 import fr.inrae.urgi.faidare.dao.XRefDocumentDao;
@@ -7,6 +13,7 @@ import fr.inrae.urgi.faidare.dao.v1.GermplasmAttributeV1Dao;
 import fr.inrae.urgi.faidare.dao.v1.GermplasmPedigreeV1Dao;
 import fr.inrae.urgi.faidare.dao.v1.GermplasmV1Dao;
 import fr.inrae.urgi.faidare.dao.v2.GermplasmMcpdDao;
+import fr.inrae.urgi.faidare.dao.v2.GermplasmV2Dao;
 import fr.inrae.urgi.faidare.domain.CollPopVO;
 import fr.inrae.urgi.faidare.domain.GermplasmMcpdVO;
 import fr.inrae.urgi.faidare.domain.XRefDocumentVO;
@@ -15,20 +22,21 @@ import fr.inrae.urgi.faidare.domain.brapi.v1.GermplasmAttributeV1VO;
 import fr.inrae.urgi.faidare.domain.brapi.v1.GermplasmAttributeValueV1VO;
 import fr.inrae.urgi.faidare.domain.brapi.v1.GermplasmPedigreeV1VO;
 import fr.inrae.urgi.faidare.domain.brapi.v1.GermplasmV1VO;
+import fr.inrae.urgi.faidare.domain.brapi.v2.GermplasmV2VO;
 import fr.inrae.urgi.faidare.utils.Sitemaps;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Controller used to display a germplasm card based on its ID.
@@ -42,6 +50,7 @@ import java.util.stream.Stream;
 public class GermplasmController {
 
     private final GermplasmV1Dao germplasmRepository;
+    private final GermplasmV2Dao germplasmV2Repository;
     private final GermplasmMcpdDao germplasmMcpdRepository;
     private final GermplasmPedigreeV1Dao germplasmPedigreeRepository;
     private final FaidareProperties faidareProperties;
@@ -49,16 +58,20 @@ public class GermplasmController {
     private final GermplasmAttributeV1Dao germplasmAttributeRepository;
     private final GermplasmMcpdExportService germplasmMcpdExportService;
     private final GermplasmExportService germplasmExportService;
+    private final GermplasmMiappeExportService germplasmMiappeExportService;
 
     public GermplasmController(GermplasmV1Dao germplasmRepository,
+                               GermplasmV2Dao germplasmV2Repository,
                                GermplasmMcpdDao germplasmMcpdRepository,
                                GermplasmPedigreeV1Dao germplasmPedigreeRepository,
                                FaidareProperties faidareProperties,
                                XRefDocumentDao xRefDocumentRepository,
                                GermplasmAttributeV1Dao germplasmAttributeRepository,
                                GermplasmMcpdExportService germplasmMcpdExportService,
-                               GermplasmExportService germplasmExportService) {
+                               GermplasmExportService germplasmExportService,
+                               GermplasmMiappeExportService germplasmMiappeExportService) {
         this.germplasmRepository = germplasmRepository;
+        this.germplasmV2Repository = germplasmV2Repository;
         this.germplasmMcpdRepository = germplasmMcpdRepository;
         this.germplasmPedigreeRepository = germplasmPedigreeRepository;
         this.faidareProperties = faidareProperties;
@@ -66,6 +79,7 @@ public class GermplasmController {
         this.germplasmAttributeRepository = germplasmAttributeRepository;
         this.germplasmMcpdExportService = germplasmMcpdExportService;
         this.germplasmExportService = germplasmExportService;
+        this.germplasmMiappeExportService = germplasmMiappeExportService;
     }
 
     @GetMapping("/{germplasmId}")
@@ -111,7 +125,7 @@ public class GermplasmController {
                 germplasmMcpdExportService.export(out, stream, fields);
             }
         };
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType("text/csv")).body(body);
+        return ResponseEntity.ok().contentType(ExportFormat.CSV.getMediaType()).body(body);
     }
 
     @PostMapping("/exports/plant-material")
@@ -124,7 +138,21 @@ public class GermplasmController {
                 germplasmExportService.export(out, stream, fields);
             }
         };
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType("text/csv")).body(body);
+        return ResponseEntity.ok().contentType(ExportFormat.CSV.getMediaType()).body(body);
+    }
+
+    @PostMapping("/exports/miappe")
+    @ResponseBody
+    public ResponseEntity<StreamingResponseBody> export(@Validated @RequestBody GermplasmMiappeExportCommand command) {
+        StreamingResponseBody body = out -> {
+            try (Stream<GermplasmV2VO> stream = germplasmV2Repository.findByGermplasmDbIdIn(command.ids())) {
+                switch (command.format()) {
+                    case EXCEL -> germplasmMiappeExportService.exportAsExcel(out, stream);
+                    case CSV -> germplasmMiappeExportService.exportAsCsv(out, stream);
+                }
+            }
+        };
+        return ResponseEntity.ok().contentType(command.format().getMediaType()).body(body);
     }
 
     @GetMapping("/sitemap-{index}.txt")

backend/src/test/java/fr/inrae/urgi/faidare/web/germplasm/GermplasmControllerTest.java
@@ -1,6 +1,7 @@
 package fr.inrae.urgi.faidare.web.germplasm;
 
 import static fr.inrae.urgi.faidare.web.Fixtures.htmlContent;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -8,6 +9,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,12 +22,17 @@ import fr.inrae.urgi.faidare.dao.v1.GermplasmAttributeV1Dao;
 import fr.inrae.urgi.faidare.dao.v1.GermplasmPedigreeV1Dao;
 import fr.inrae.urgi.faidare.dao.v1.GermplasmV1Dao;
 import fr.inrae.urgi.faidare.dao.v2.GermplasmMcpdDao;
+import fr.inrae.urgi.faidare.dao.v2.GermplasmV2Dao;
 import fr.inrae.urgi.faidare.domain.GermplasmMcpdVO;
 import fr.inrae.urgi.faidare.domain.XRefDocumentVO;
 import fr.inrae.urgi.faidare.domain.brapi.GermplasmSitemapVO;
 import fr.inrae.urgi.faidare.domain.brapi.v1.GermplasmAttributeV1VO;
 import fr.inrae.urgi.faidare.domain.brapi.v1.GermplasmV1VO;
+import fr.inrae.urgi.faidare.domain.brapi.v2.GermplasmV2VO;
 import fr.inrae.urgi.faidare.web.Fixtures;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +48,7 @@ import org.springframework.test.web.servlet.MvcResult;
  * @author JB Nizet
  */
 @WebMvcTest(GermplasmController.class)
-@Import({GermplasmMcpdExportService.class, GermplasmExportService.class})
+@Import({GermplasmMcpdExportService.class, GermplasmExportService.class, GermplasmMiappeExportService.class})
 public class GermplasmControllerTest {
 
     @Autowired
@@ -52,6 +59,9 @@ public class GermplasmControllerTest {
 
     @MockitoBean
     private GermplasmV1Dao mockGermplasmRepository;
+
+    @MockitoBean
+    private GermplasmV2Dao mockGermplasmV2Repository;
 
     @MockitoBean
     private FaidareProperties mockFaidareProperties;
@@ -239,7 +249,7 @@ public class GermplasmControllerTest {
 
         this.mockMvc.perform(asyncDispatch(mvcResult))
                .andExpect(status().isOk())
-               .andExpect(content().contentType("text/csv"))
+               .andExpect(content().contentType(ExportFormat.CSV.getMediaType()))
                .andExpect(content().string("\"PUID\";\"INSTCODE\"\n" +
                                                "\"PUI1\";\"Inst1\"\n" +
                                                "\"PUI1\";\"Inst1\"\n"));
@@ -271,11 +281,76 @@ public class GermplasmControllerTest {
 
         this.mockMvc.perform(asyncDispatch(mvcResult))
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType("text/csv"))
+                    .andExpect(content().contentType(ExportFormat.CSV.getMediaType()))
                     .andExpect(content().string("\"DOI\";\"Accession number\";\"Accession name\"\n" +
                                                     "\"germplasmPUI\";\"1408\";\"BLE BARBU DU ROUSSILLON\"\n" +
                                                     "\"germplasmPUI\";\"1408\";\"BLE BARBU DU ROUSSILLON\"\n" +
                                                     "\"germplasmPUI mini\";\"1408-mini\";\"BLE BARBU DU ROUSSILLON mini\"\n"));
+    }
+
+    @Test
+    void shouldExportMiappeAsExcel() throws Exception {
+        List<GermplasmV2VO> germplasms = List.of(
+            Fixtures.createGermplasmV2ForTrial(),
+            Fixtures.createGermplasmV2ForTrial()
+        );
+
+        GermplasmMiappeExportCommand command = new GermplasmMiappeExportCommand(
+            Sets.newHashSet("g1", "g2"),
+            ExportFormat.EXCEL
+        );
+
+        when(mockGermplasmV2Repository.findByGermplasmDbIdIn(eq(command.ids())))
+            .thenAnswer(invocation -> germplasms.stream());
+
+        MvcResult mvcResult = mockMvc.perform(post("/germplasms/exports/miappe")
+                                                  .contentType(MediaType.APPLICATION_JSON)
+                                                  .content(objectMapper.writeValueAsBytes(command)))
+                                     .andExpect(request().asyncStarted())
+                                     .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(ExportFormat.EXCEL.getMediaType()))
+                    .andExpect(result -> {
+                        byte[] content = result.getResponse().getContentAsByteArray();
+                        try (XSSFWorkbook workbook = new XSSFWorkbook(new ByteArrayInputStream(content))) {
+                            assertThat(workbook.getNumberOfSheets()).isEqualTo(1);
+                            XSSFSheet sheet = workbook.getSheetAt(0);
+                            assertThat(sheet.getPhysicalNumberOfRows()).isEqualTo(3); // header + 2 germplasms
+                        }
+                    });
+    }
+
+    @Test
+    void shouldExportMiappeAsCsv() throws Exception {
+        List<GermplasmV2VO> germplasms = List.of(
+            Fixtures.createGermplasmV2ForTrial(),
+            Fixtures.createGermplasmV2ForTrial()
+        );
+
+        GermplasmMiappeExportCommand command = new GermplasmMiappeExportCommand(
+            Sets.newHashSet("g1", "g2"),
+            ExportFormat.CSV
+        );
+
+        when(mockGermplasmV2Repository.findByGermplasmDbIdIn(eq(command.ids())))
+            .thenAnswer(invocation -> germplasms.stream());
+
+        MvcResult mvcResult = mockMvc.perform(post("/germplasms/exports/miappe")
+                                                  .contentType(MediaType.APPLICATION_JSON)
+                                                  .content(objectMapper.writeValueAsBytes(command)))
+                                     .andExpect(request().asyncStarted())
+                                     .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(ExportFormat.CSV.getMediaType()))
+                    .andExpect(result -> {
+                        String content = result.getResponse().getContentAsString();
+                        List<String> lines = content.lines().toList();
+                        assertThat(lines).hasSize(3); // header + 2 germplasms
+                    });
     }
 
     private void testSitemap(int index, String expectedContent) throws Exception {

elixir-europe/plant-faidare

backend/src/main/java/fr/inrae/urgi/faidare/web/germplasm/GermplasmController.java
@@ -1,5 +1,11 @@
 package fr.inrae.urgi.faidare.web.germplasm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import fr.inrae.urgi.faidare.api.NotFoundException;
 import fr.inrae.urgi.faidare.config.FaidareProperties;
 import fr.inrae.urgi.faidare.dao.XRefDocumentDao;
@@ -21,15 +27,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Controller used to display a germplasm card based on its ID.
@@ -49,6 +55,7 @@ public class GermplasmController {
     private final GermplasmAttributeV1Dao germplasmAttributeRepository;
     private final GermplasmMcpdExportService germplasmMcpdExportService;
     private final GermplasmExportService germplasmExportService;
+    private final GermplasmMiappeExportService germplasmMiappeExportService;
 
     public GermplasmController(GermplasmV2Dao germplasmRepository,
                                GermplasmMcpdDao germplasmMcpdRepository,
@@ -57,7 +64,8 @@ public class GermplasmController {
                                XRefDocumentDao xRefDocumentRepository,
                                GermplasmAttributeV1Dao germplasmAttributeRepository,
                                GermplasmMcpdExportService germplasmMcpdExportService,
-                               GermplasmExportService germplasmExportService) {
+                               GermplasmExportService germplasmExportService,
+                               GermplasmMiappeExportService germplasmMiappeExportService) {
         this.germplasmRepository = germplasmRepository;
         this.germplasmMcpdRepository = germplasmMcpdRepository;
         this.germplasmPedigreeRepository = germplasmPedigreeRepository;
@@ -66,6 +74,7 @@ public class GermplasmController {
         this.germplasmAttributeRepository = germplasmAttributeRepository;
         this.germplasmMcpdExportService = germplasmMcpdExportService;
         this.germplasmExportService = germplasmExportService;
+        this.germplasmMiappeExportService = germplasmMiappeExportService;
     }
 
     @GetMapping("/{germplasmId}")
@@ -111,7 +120,7 @@ public class GermplasmController {
                 germplasmMcpdExportService.export(out, stream, fields);
             }
         };
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType("text/csv")).body(body);
+        return ResponseEntity.ok().contentType(ExportFormat.CSV.getMediaType()).body(body);
     }
 
     @PostMapping("/exports/plant-material")
@@ -124,7 +133,21 @@ public class GermplasmController {
                 germplasmExportService.export(out, stream, fields);
             }
         };
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType("text/csv")).body(body);
+        return ResponseEntity.ok().contentType(ExportFormat.CSV.getMediaType()).body(body);
+    }
+
+    @PostMapping("/exports/miappe")
+    @ResponseBody
+    public ResponseEntity<StreamingResponseBody> export(@Validated @RequestBody GermplasmMiappeExportCommand command) {
+        StreamingResponseBody body = out -> {
+            try (Stream<GermplasmV2VO> stream = germplasmRepository.findByGermplasmDbIdIn(command.ids())) {
+                switch (command.format()) {
+                    case EXCEL -> germplasmMiappeExportService.exportAsExcel(out, stream);
+                    case CSV -> germplasmMiappeExportService.exportAsCsv(out, stream);
+                }
+            }
+        };
+        return ResponseEntity.ok().contentType(command.format().getMediaType()).body(body);
     }
 
     @GetMapping("/sitemap-{index}.txt")

backend/src/test/java/fr/inrae/urgi/faidare/web/germplasm/GermplasmControllerTest.java
@@ -1,6 +1,7 @@
 package fr.inrae.urgi.faidare.web.germplasm;
 
 import static fr.inrae.urgi.faidare.web.Fixtures.htmlContent;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -8,6 +9,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,6 +27,8 @@ import fr.inrae.urgi.faidare.domain.brapi.GermplasmSitemapVO;
 import fr.inrae.urgi.faidare.domain.brapi.v1.GermplasmAttributeV1VO;
 import fr.inrae.urgi.faidare.domain.brapi.v2.GermplasmV2VO;
 import fr.inrae.urgi.faidare.web.Fixtures;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +45,7 @@ import tools.jackson.databind.ObjectMapper;
  * @author JB Nizet
  */
 @WebMvcTest(GermplasmController.class)
-@Import({GermplasmMcpdExportService.class, GermplasmExportService.class})
+@Import({GermplasmMcpdExportService.class, GermplasmExportService.class, GermplasmMiappeExportService.class})
 public class GermplasmControllerTest {
 
     @Autowired
@@ -239,7 +243,7 @@ public class GermplasmControllerTest {
 
         this.mockMvc.perform(asyncDispatch(mvcResult))
                .andExpect(status().isOk())
-               .andExpect(content().contentType("text/csv"))
+               .andExpect(content().contentType(ExportFormat.CSV.getMediaType()))
                .andExpect(content().string("\"PUID\";\"INSTCODE\"\n" +
                                                "\"PUI1\";\"Inst1\"\n" +
                                                "\"PUI1\";\"Inst1\"\n"));
@@ -271,11 +275,76 @@ public class GermplasmControllerTest {
 
         this.mockMvc.perform(asyncDispatch(mvcResult))
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType("text/csv"))
+                    .andExpect(content().contentType(ExportFormat.CSV.getMediaType()))
                     .andExpect(content().string("\"DOI\";\"Accession number\";\"Accession name\"\n" +
                                                     "\"germplasmPUI\";\"1408\";\"BLE BARBU DU ROUSSILLON\"\n" +
                                                     "\"germplasmPUI\";\"1408\";\"BLE BARBU DU ROUSSILLON\"\n" +
                                                     "\"germplasmPUI mini\";\"1408-mini\";\"BLE BARBU DU ROUSSILLON mini\"\n"));
+    }
+
+    @Test
+    void shouldExportMiappeAsExcel() throws Exception {
+        List<GermplasmV2VO> germplasms = List.of(
+            Fixtures.createGermplasmV2ForTrial(),
+            Fixtures.createGermplasmV2ForTrial()
+        );
+
+        GermplasmMiappeExportCommand command = new GermplasmMiappeExportCommand(
+            Sets.newHashSet("g1", "g2"),
+            ExportFormat.EXCEL
+        );
+
+        when(mockGermplasmRepository.findByGermplasmDbIdIn(eq(command.ids())))
+            .thenAnswer(invocation -> germplasms.stream());
+
+        MvcResult mvcResult = mockMvc.perform(post("/germplasms/exports/miappe")
+                                                  .contentType(MediaType.APPLICATION_JSON)
+                                                  .content(objectMapper.writeValueAsBytes(command)))
+                                     .andExpect(request().asyncStarted())
+                                     .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(ExportFormat.EXCEL.getMediaType()))
+                    .andExpect(result -> {
+                        byte[] content = result.getResponse().getContentAsByteArray();
+                        try (XSSFWorkbook workbook = new XSSFWorkbook(new ByteArrayInputStream(content))) {
+                            assertThat(workbook.getNumberOfSheets()).isEqualTo(1);
+                            XSSFSheet sheet = workbook.getSheetAt(0);
+                            assertThat(sheet.getPhysicalNumberOfRows()).isEqualTo(3); // header + 2 germplasms
+                        }
+                    });
+    }
+
+    @Test
+    void shouldExportMiappeAsCsv() throws Exception {
+        List<GermplasmV2VO> germplasms = List.of(
+            Fixtures.createGermplasmV2ForTrial(),
+            Fixtures.createGermplasmV2ForTrial()
+        );
+
+        GermplasmMiappeExportCommand command = new GermplasmMiappeExportCommand(
+            Sets.newHashSet("g1", "g2"),
+            ExportFormat.CSV
+        );
+
+        when(mockGermplasmRepository.findByGermplasmDbIdIn(eq(command.ids())))
+            .thenAnswer(invocation -> germplasms.stream());
+
+        MvcResult mvcResult = mockMvc.perform(post("/germplasms/exports/miappe")
+                                                  .contentType(MediaType.APPLICATION_JSON)
+                                                  .content(objectMapper.writeValueAsBytes(command)))
+                                     .andExpect(request().asyncStarted())
+                                     .andReturn();
+
+        this.mockMvc.perform(asyncDispatch(mvcResult))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(ExportFormat.CSV.getMediaType()))
+                    .andExpect(result -> {
+                        String content = result.getResponse().getContentAsString();
+                        List<String> lines = content.lines().toList();
+                        assertThat(lines).hasSize(3); // header + 2 germplasms
+                    });
     }
 
     private void testSitemap(int index, String expectedContent) throws Exception {